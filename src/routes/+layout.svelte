a558f7fca3c9ee8e0ca1629cf30367fbc98be914
Behnam <behnam.h1@gmail.com>
2023-02-20 11:11:03 +0200

    font and new structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,31 +2,12 @@
 	import '../app.css';
 </script>
 
-<div class="w-full h-screen bg-slate-200 flex flex-col p-14">
-	<nav>
-		<h1 class="text-4xl uppercase">Alpaca Generator</h1>
-	</nav>
-	<main class="h-60 flex flex-row">
-		<div class="flex flex-col w-1/2">
-			<div class="w-full">
-				<p>picture</p>
-			</div>
-			<div class="flex flex-row">
-				<p>Random</p>
-				<p>Download</p>
-			</div>
-		</div>
-		<div class="flex flex-col w-1/2">
-			<h1>Options</h1>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, facilis!</p>
-			<h1>Options</h1>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, facilis!</p>
-		</div>
-	</main>
-</div>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
 
-<style>
-	* {
-		border: solid;
-	}
-</style>
+<div class="w-full h-screen bg-slate-200 flex flex-col gap-y-4">
+	<div class="py-12 px-20 w-10/12 mx-auto">
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Nav from "$lib/components/Nav.svelte"
+  import PictureFrame from "$lib/components/PictureFrame.svelte"
+  import Option from "$lib/components/Option.svelte"
+</script>
+
+<!-- TODO: align with image -->
+<!-- TODO: add hover effect -->
+<Nav />
+<main class="flex flex-row mt-5 gap-4">
+  <div class="flex-col w-1/2">
+    <div class="">
+      <PictureFrame />
+    </div>
+    <div class="flex flex-row justify-center mt-5">
+      <button class="bg-white rounded-md px-10 py-4 font-medium mx-4">🔀 Random</button>
+      <button class="bg-white rounded-md px-10 py-4 font-medium mx-4">🖼️ Download</button>
+    </div>
+  </div>
+  <div class="flex flex-col w-1/2">
+    <!-- TODO: replace with components -->
+    <!-- TODO: add hover effect -->
+    <h2>ACCCESSORIZE THE ALPACA'S</h2>
+    <div class="flex flex-row flex-wrap gap-2 my-3">
+      <Option title="Ears" />
+      <Option title="Eyes" />
+      <Option title="Mouth" />
+      <Option title="Neck" />
+      <Option title="Neck" />
+      <Option title="Neck" />
+    </div>
+    <!-- TODO: replace with components -->
+    <h2 class="mt-6">STYLE</h2>
+    <div class="flex flex-row flex-wrap gap-2 my-3">
+      <Option title="Hair"/>
+      <Option title="Ears"/>
+      <Option title="Eyes"/>
+      <Option title="Mouth"/>
+      <Option title="Neck"/>
+      <Option title="Neck"/>
+      <Option title="Neck"/>
+    </div>
+  </div>
+</main>
