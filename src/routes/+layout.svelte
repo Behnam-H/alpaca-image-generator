fcfa2bb8abcb5e48260ebe36b5679b859a5c8fa0
Behnam <behnam.h1@gmail.com>
2023-02-27 23:30:41 +0200

    added title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,15 @@
 	import '../app.css';
 </script>
 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
-
+<head>
+	<title>Alpaca generator</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Roboto+Condensed:wght@700&display=swap"
+		rel="stylesheet"
+	/>
+</head>
 <div class="w-full h-screen bg-slate-200 flex flex-col gap-y-4">
 	<div class="py-12 px-20 w-10/12 mx-auto">
 		<slot />
