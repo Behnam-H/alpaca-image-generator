f5e00ae8099e0aa75aa700eaab6e2e2341e66aab
Behnam <behnam.h1@gmail.com>
2023-02-27 23:30:29 +0200

    dynamic style

--- src/lib/components/Option.svelte
+++ src/lib/components/Option.svelte
@@ -1,4 +1,15 @@
 <script>
   export let title = ''
+  let hovering = false
+
+  function enter() {
+    hovering = true
+  }
+  function leave(){
+    hovering = false
+  }
 </script>
-<button class="bg-transparent border-2 border-solid border-sky-400 py-2 px-6 rounded-3xl text-sky-500">{title}</button>
+<button class="{hovering ? 'bg-sky-400 text-white' : 'bg-transparent text-sky-400'} border-2 border-solid border-sky-400 py-2 px-6 rounded-3xl" on:mouseenter={enter} on:mouseleave={leave}>
+  {title}
+</button>
+
