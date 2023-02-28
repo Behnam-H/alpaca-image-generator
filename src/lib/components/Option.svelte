91118f076c560b9267acc2e6d2ab47818680c565
Behnam <behnam.h1@gmail.com>
2023-02-28 12:53:37 +0200

    highlight options

--- src/lib/components/Option.svelte
+++ src/lib/components/Option.svelte
@@ -1,15 +1,27 @@
 <script>
-  export let title = ''
-  let hovering = false
-
-  function enter() {
-    hovering = true
-  }
-  function leave(){
-    hovering = false
-  }
+	import { parts } from "../../stores";
+	import { get } from "svelte/store";
+	export let title = '';
+	let hovering = false;
+  export let selectedAttribute = '';
+  export let selectedOption = '';
+  $: selected = selectedAttribute == title || selectedOption == title
+	function enter() {
+		hovering = true;
+	}
+	function leave() {
+		hovering = false;
+	}
+	// TODO: Highlight chosen option
 </script>
-<button class="{hovering ? 'bg-sky-400 text-white' : 'bg-transparent text-sky-400'} border-2 border-solid border-sky-400 py-2 px-6 rounded-3xl" on:mouseenter={enter} on:mouseleave={leave}>
-  {title}
-</button>
 
+<button
+	class="{hovering || selected
+		? 'bg-sky-500 text-white'
+		: 'bg-transparent text-sky-500'} capitalize border-2 border-solid border-sky-500 py-2 px-6 rounded-3xl"
+	on:mouseenter={enter}
+	on:mouseleave={leave}
+  on:click
+>
+	{title}
+</button>
