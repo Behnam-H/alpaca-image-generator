87dd26b247249f304763da604f149696ef43f2e6
Behnam <behnam.h1@gmail.com>
2023-03-01 10:28:42 +0200

    dynamic options

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,70 @@
 <script>
-  import Nav from "$lib/components/Nav.svelte"
-  import PictureFrame from "$lib/components/PictureFrame.svelte"
-  import Option from "$lib/components/Option.svelte"
+	import { onMount } from 'svelte';
+	import { parts } from '../stores';
+	import Nav from '$lib/components/Nav.svelte';
+	import PictureFrame from '$lib/components/PictureFrame.svelte';
+	import Option from '$lib/components/Option.svelte';
+	import data from '$lib/data.json';
+	let selectedAttribute = '';
+	let selectedOption = '';
+
+	function handleAttrSelection(attribute) {
+		selectedAttribute = attribute;
+	}
+	function handleOptSelection(opt) {
+		selectedOption = opt;
+    parts.update(oldParts => { 
+			return {...oldParts, [selectedAttribute]: selectedOption}
+    })
+	}
+
+	onMount(() => {
+		parts.set({
+			accessories: data.accessories[Math.floor(Math.random() * data.accessories.length)],
+			backgrounds: data.backgrounds[Math.floor(Math.random() * data.backgrounds.length)],
+			ears: data.ears[Math.floor(Math.random() * data.ears.length)],
+			eyes: data.eyes[Math.floor(Math.random() * data.eyes.length)],
+			hair: data.hair[Math.floor(Math.random() * data.hair.length)],
+			leg: data.leg[Math.floor(Math.random() * data.leg.length)],
+			mouth: data.mouth[Math.floor(Math.random() * data.mouth.length)],
+			neck: data.neck[Math.floor(Math.random() * data.neck.length)]
+		});
+	});
 </script>
 
-<!-- TODO: align with image -->
-<!-- TODO: add hover effect -->
+<!-- TODO: update structure -->
 <Nav />
 <main class="flex flex-row mt-5 gap-4">
-  <div class="flex-col w-1/2">
-    <div class="">
-      <PictureFrame />
-    </div>
-    <div class="flex flex-row justify-center mt-5">
-      <button class="bg-white rounded-md px-10 py-4 font-medium mx-4">🔀 Random</button>
-      <button class="bg-white rounded-md px-10 py-4 font-medium mx-4">🖼️ Download</button>
-    </div>
-  </div>
-  <div class="flex flex-col w-1/2">
-    <!-- TODO: replace with components -->
-    <!-- TODO: add hover effect -->
-    <h2>ACCCESSORIZE THE ALPACA'S</h2>
-    <div class="flex flex-row flex-wrap gap-2 my-3">
-      <Option title="Ears" />
-      <Option title="Eyes" />
-      <Option title="Mouth" />
-      <Option title="Neck" />
-      <Option title="Neck" />
-      <Option title="Neck" />
-    </div>
-    <!-- TODO: replace with components -->
-    <h2 class="mt-6">STYLE</h2>
-    <div class="flex flex-row flex-wrap gap-2 my-3">
-      <Option title="Hair"/>
-      <Option title="Ears"/>
-      <Option title="Eyes"/>
-      <Option title="Mouth"/>
-      <Option title="Neck"/>
-      <Option title="Neck"/>
-      <Option title="Neck"/>
-    </div>
-  </div>
+	<div class="flex-col w-1/2">
+		<div>
+			<PictureFrame />
+		</div>
+		<div class="flex flex-row justify-center mt-5">
+			<button class="bg-white rounded-md px-10 py-4 font-medium mx-4">🔀 Random</button>
+			<button class="bg-white rounded-md px-10 py-4 font-medium mx-4">🖼️ Download</button>
+		</div>
+	</div>
+	<div class="flex flex-col w-1/2">
+		<h2>ACCCESSORIZE THE ALPACA'S</h2>
+		<div class="flex flex-row flex-wrap gap-2 my-3">
+			{#each Object.keys(data) as attribute}
+				<Option
+					title={attribute}
+					on:click={() => handleAttrSelection(attribute)}
+					{selectedAttribute}
+				/>
+			{/each}
+		</div>
+		<!-- TODO: Highlight selected opt  -->
+		<h2 class="mt-6">STYLE</h2>
+		<div class="flex flex-row flex-wrap gap-2 my-3">
+			{#if selectedAttribute}
+				{#each data[selectedAttribute] as option}
+					<Option title={option} on:click={() => handleOptSelection(option)} {selectedOption} />
+				{/each}
+			{:else}
+						<p>First pick an accessory</p>
+			{/if}
+		</div>
+	</div>
 </main>
